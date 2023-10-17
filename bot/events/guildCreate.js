8fb95d064e798e67f03ea1a060b970713bacdd89
SAASMEEM <46927159+SAASMEEM@users.noreply.github.com>
2023-10-17 10:56:56 +0200

    autofix lint

--- bot/events/guildCreate.js
+++ bot/events/guildCreate.js
@@ -2,57 +2,59 @@ const { Events } = require("discord.js");
 const { mariaConnect } = require("../functions/mariadb-connection");
 const mysql = require("mysql2");
 
-
 module.exports = {
-    name: Events.GuildCreate,
-    async execute(guild) {
-        console.log(`Joined guild: ${guild.name} with ID: ${guild.id}`);
+	name: Events.GuildCreate,
+	async execute(guild) {
+		console.log(`Joined guild: ${guild.name} with ID: ${guild.id}`);
 
-        var guildExists = false
+		var guildExists = false;
 
-        const query = `SELECT * FROM guilds WHERE id = ${guild.id}`;
+		const query = `SELECT * FROM guilds WHERE id = ${guild.id}`;
 
-        const checkIfGuildExists = () => {
-            return new Promise((resolve, reject) => {
-                mariaConnect((connection) => {
-                    connection.query(query, (error, results, fields) => {
-                        if (error) {
-                            console.error("Error executing query:", error);
-                            reject(error);
-                        } else {
-                            if (results.length > 0) {
-                                console.log('Guild entry with matching ID found.');
-                                guildExists = true;
-                            }
-                            resolve();
-                        }
-                    });
-                });
-            });
-        };
+		const checkIfGuildExists = () => {
+			return new Promise((resolve, reject) => {
+				mariaConnect((connection) => {
+					connection.query(query, (error, results, fields) => {
+						if (error) {
+							console.error("Error executing query:", error);
+							reject(error);
+						} else {
+							if (results.length > 0) {
+								console.log("Guild entry with matching ID found.");
+								guildExists = true;
+							}
+							resolve();
+						}
+					});
+				});
+			});
+		};
 
-        const insertGuildIfNotExists = () => {
-            if (!guildExists) {
-                const data = { id: guild.id, name: guild.name };
+		const insertGuildIfNotExists = () => {
+			if (!guildExists) {
+				const data = { id: guild.id, name: guild.name };
 
-                mariaConnect((connection) => {
-                    connection.query("INSERT INTO guilds SET ?", data, (error, results, fields) => {
-                        if (error) {
-                            console.error("Error executing query:", error);
-                        } else {
-                            console.log('Inserted ' + results.affectedRows + ' row(s).');
-                        }
-                    });
-                });
-            }
-        };
+				mariaConnect((connection) => {
+					connection.query(
+						"INSERT INTO guilds SET ?",
+						data,
+						(error, results, fields) => {
+							if (error) {
+								console.error("Error executing query:", error);
+							} else {
+								console.log("Inserted " + results.affectedRows + " row(s).");
+							}
+						}
+					);
+				});
+			}
+		};
 
-        try {
-            await checkIfGuildExists();
-            insertGuildIfNotExists();
-        } catch (error) {
-            console.error("Error occurred:", error);
-        }
-
-    },
+		try {
+			await checkIfGuildExists();
+			insertGuildIfNotExists();
+		} catch (error) {
+			console.error("Error occurred:", error);
+		}
+	},
 };
